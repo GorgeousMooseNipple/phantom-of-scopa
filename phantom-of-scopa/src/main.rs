fe076abaa0b5e9eb84cc00470afc0d7866da85ad
GorgeousMooseNipple <dkorneev.sc@gmail.com>
2024-02-29 13:38:44 +0300

    Create basic bevy desktop app

--- phantom-of-scopa/src/main.rs
+++ phantom-of-scopa/src/main.rs
@@ -1,3 +1,42 @@
+mod game;
+use bevy::prelude::*;
+use bevy::window::{PresentMode, WindowTheme};
+
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+enum AppState {
+    #[default]
+    InGame,
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Phantom of Scopa".into(),
+                resolution: (800., 481.).into(),
+                present_mode: PresentMode::AutoVsync,
+                prevent_default_event_handling: false,
+                window_theme: Some(WindowTheme::Dark),
+                enabled_buttons: bevy::window::EnabledButtons {
+                    maximize: false,
+                    ..Default::default()
+                },
+                ..default()
+            }),
+            ..default()
+        }))
+        .init_state::<AppState>()
+        .add_plugins(game::game_plugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in to_despawn.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
 }
