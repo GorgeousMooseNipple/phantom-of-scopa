8db5dce9de4be4bf9f8508df979a2b950a3372c2
GorgeousMooseNipple <dkorneev.sc@gmail.com>
2024-03-04 19:46:05 +0300

    Change pop-ups background to non-transparent

--- phantom-of-scopa/src/game/popups.rs
+++ phantom-of-scopa/src/game/popups.rs
@@ -1,4 +1,4 @@
-use super::constants::*;
+use super::styles::*;
 use bevy::prelude::*;
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
@@ -70,7 +70,7 @@ pub fn handle_popups(
                         margin: UiRect::vertical(Val::Px(10.0)),
                         ..default()
                     },
-                    background_color: BackgroundColor(POPUP_BG),
+                    background_color: BackgroundColor(MENU_BG),
                     ..default()
                 },
             ))
